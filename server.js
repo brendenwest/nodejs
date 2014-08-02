c82415c90b40ddef3d1de566cb1cd8b6d8626a7a
Brendenw <brendenw@hotmail.com>
2014-08-02 11:33:26 -0700

    adding trends

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var http = require("http");
 var url = require('url'); 
 var api = require("./api");
+var trends = require("./trends");
 
 function start() {
   function onRequest(request, response) {
   	var pathname = url.parse(request.url).pathname;
 	switch(pathname) {
-	case '/getjobs':
-		api.getjobs(request, response);
-
-	break;
-	default:
-    	response.writeHead(200, {"Content-Type": "text/plain"});
-    	response.write("Hello World");
-    	response.end();
-	}
+  	case '/getjobs':
+  		api.getjobs(request, response);
+      break;
+    case '/getsalaries':
+      trends.getsalaries(request, response);
+    	break;
+  	default:
+      	response.writeHead(200, {"Content-Type": "text/plain"});
+      	response.write("Hello World");
+      	response.end();
+  	}
   }
 
   http.createServer(onRequest).listen(process.env.PORT || 8888);
---
+++ trends.js
@@ -0,0 +1,50 @@
+// index.js
+var request = require('request');
+var url = require('url'); 
+var cheerio = require('cheerio');
+
+
+function getsalaries(req, response) {
+    var queryData = url.parse(req.url,true).query; 
+    var titles = [];
+    var jobsArray = [];
+
+    var location = (queryData["location"]) ? '-l-' +queryData["loc"] : '';
+
+    var dataUrl = 'http://www.simplyhired.com/salaries-k-' +queryData["kw"] +location+ '-jobs.html';
+//    http://www.simplyhired.com/salaries-k-php-l-98115-jobs.html
+    request(dataUrl, (function() {
+      return function(err, resp, body) {
+        if (err)
+            throw err;
+        $ = cheerio.load(body);
+        // get job titles
+        $(' .SH_job_title').each(function() {
+            titles.push($(this).text());
+        });
+        i=0;
+        // get job salaries
+        $(' .SH_salary').each(function() {
+            jobsArray.push({
+                "title" : titles[i],
+                "salary" : $(this).text()
+             });
+
+            i++;
+        }); 
+        jobsArray.push({ "updated" : $(' .SH_bar_graph_footer').first().text() });
+        jobsArray.push({ "timestamp" : new Date() });
+
+          response.writeHead(200, {'Content-Type': 'application/json'});
+          response.write(JSON.stringify(jobsArray), false, null);
+          response.end();
+      }}) ());
+}
+
+
+exports.getsalaries = getsalaries;
+
+
+// SH_bar_graph_footer, SH_job_title, SH_salary
+
+
