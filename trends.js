37fbf8a64da640ba3cf484dae23717b34a9f5a14
Brendenw <brendenw@hotmail.com>
2014-10-08 13:14:47 -0700

    get data from Indeed.com

--- trends.js
+++ trends.js
@@ -9,22 +9,36 @@ function getsalaries(req, response) {
     var titles = [];
     var jobsArray = [];
 
-    var location = (queryData["location"]) ? '-l-' +queryData["loc"] : '';
+    var location = queryData["location"];
 
-    var dataUrl = 'http://www.simplyhired.com/salaries-k-' +queryData["kw"] +location+ '-jobs.html';
-//    http://www.simplyhired.com/salaries-k-php-l-98115-jobs.html
+/*
+
+<tr class="row_1">
+                                        
+                                        <td class="col_a"><h2><a href="/q-Lead-Android-Developer-jobs.html" class="job_title">Lead Android Developer</a></h2></td>
+                    <td class="col_b"><span class="salary">$108,000</span></td>
+                    <td class="col_c" colspan="3"><div class="bar query_1" style="width: 80%;"></div></td>
+                    <!-- td class="col_e"><span class="sampleSize">330</span></td -->
+                  </tr>
+*/
+
+    var dataUrl = 'http://www.indeed.com/salary?q1=' + queryData["kw"] + '&l1=' +queryData["location"];
     request(dataUrl, (function() {
       return function(err, resp, body) {
         if (err)
             throw err;
         $ = cheerio.load(body);
         // get job titles
-        $(' .SH_job_title').each(function() {
-            titles.push($(this).text());
+        $(' .job_title').each(function() {
+            var title = $(this).text();
+            if (location) {
+              title = title.substring(0,title.indexOf(" in"));
+            }
+            titles.push(title);
         });
         i=0;
         // get job salaries
-        $(' .SH_salary').each(function() {
+        $(' .salary').each(function() {
             jobsArray.push({
                 "title" : titles[i],
                 "salary" : $(this).text()
@@ -32,12 +46,13 @@ function getsalaries(req, response) {
 
             i++;
         }); 
-        jobsArray.push({ "updated" : $(' .SH_bar_graph_footer').first().text() });
+        jobsArray.push({ "updated" : $('#salary_display_table tfoot tr th:nth-child(1) span').text() });
         jobsArray.push({ "timestamp" : new Date() });
 
           response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
 
           response.write(JSON.stringify(jobsArray), false, null);
+
           response.end();
       }}) ());
 }
