aeff14a57f47e3c281451fec7256d716762d6e8d
Brenden West <brendenw@hotmail.com>
2016-07-31 00:10:42 -0700

    Add API for tech-mentor jobs

--- meetup.js
+++ meetup.js
@@ -28,3 +28,30 @@ exports.events = function( callback) {
         });
     }).end();
 }
+
+exports.jobs = function( callback) {
+    var endpoint = "https://api.meetup.com/seattle-tech-mentors/boards/20075740/discussions?key=712d6c707945967181c1c15947504a&page=20&only=subject,created,id,started_by";
+    var now = Date.now();
+
+    return http.get(endpoint, function(response) {
+
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        
+        response.on('end', function() {
+
+            // toss jobs older than today+14
+            var all_items = JSON.parse(body);            
+            var filtered = all_items.filter(function(item){
+                var timeDiff = Math.abs(item.created - now); 
+                var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+                return diffDays <= 14;
+            });
+
+            callback(filtered);
+        });
+    }).end();
+}
